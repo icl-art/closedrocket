4b1b24b2bcc6e2d9f3a713bbb7bc0ed4abb6e398
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-10-30 23:09:10 +0100

    Openrocket no longer saves files as zips

--- core/src/net/sf/openrocket/appearance/defaults/ResourceDecalImage.java
+++ core/src/net/sf/openrocket/appearance/defaults/ResourceDecalImage.java
@@ -48,7 +48,12 @@ public class ResourceDecalImage implements DecalImage {
 	@Override
 	public void fireChangeEvent(Object source) {
 	}
-	
+
+	@Override
+	public File getFileSystemLocation() {
+		return null;
+	}
+
 	@Override
 	public void addChangeListener(StateChangeListener listener) {
 		//Unimplemented, this can not change
